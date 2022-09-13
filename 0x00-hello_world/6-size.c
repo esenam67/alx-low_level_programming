64cbd98d550f6443d547b7c7f735d500edb8caf1
esenam67 <moneyesenam@gmail.com>
2022-09-12 21:13:37 -0700

    yh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,7 @@
 
 
 /**
-
+ *
  * main -print the string in the put fonction
  *
  *Description: using the main function
@@ -12,17 +12,16 @@
  */
 int main(void)
 {
-    char c;
-    int i;
-    long li;
-    long long lli;
-    float f;
-    
-    printf("size of a char: %ld byte(s)\n", sizeof(c));
-     printf("size of an int: %ld byte(s)\n", sizeof(i));
-      printf("size of a long int: %ld byte(s)\n", sizeof(li));
-       printf("size of a long long: %ld byte(s)\n", sizeof(lli));
-        printf("size of a float: %ld byte(s)\n", sizeof(f));
-    return (0);
+char c;
+int i;
+long li;
+long long lli;
+float f;
+printf("size of a char: %ld byte(s)\n", sizeof(c));
+printf("size of an int: %ld byte(s)\n", sizeof(i));
+printf("size of a long int: %ld byte(s)\n", sizeof(li));
+printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("size of a float: %ld byte(s)\n", sizeof(f));
+return (0);
 
 }
