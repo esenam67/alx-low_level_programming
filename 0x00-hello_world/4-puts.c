d7d7e8ac0d98e794027842a1b583c9e70e148606
esenam67 <moneyesenam@gmail.com>
2022-09-12 20:36:20 -0700

    task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,19 +3,15 @@
 
 
 /**
- *
  *  * main - Entry point
  *
- *   *
- *
  *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
 {
-       puts("\"Programming is like building a multilingual puzzle"); 
+puts("\"Programming is like building a multilingual puzzle");
 	    return (0);
 
 }
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+/**
+
+ * main -print the string in the put fonction
+ *
+ *Description: using the main function
+ *this program prints
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+    init i;
+    long li;
+    long long lli;
+    float f;
+    
+    printf("size of a char: %ld byte(s)\n", sizeof(c));
+     printf("size of an int: %ld byte(s)\n", sizeof(i));
+      printf("size of a long int: %ld byte(s)\n", sizeof(li));
+       printf("size of a long long: %ld byte(s)\n", sizeof(lli));
+        printf("size of a float: %ld byte(s)\n", sizeof(f));
+    return (0);
+
+}
