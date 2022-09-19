df0277f9ee19d22e6c6ffe7ea77ad92151de7a62
esenam67 <moneyesenam@gmail.com>
2022-09-19 04:53:51 -0700

    yo

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
 int main(void)
 {
 char alphab;
-for (alphab = 'a' ; alphab <= 'z' ; alphab++);
+for (alphab = 'a' ; alphab <= 'z' ; alphab++)
 {
 putchar(alphab);
 }
-for (alphab = 'A' ; alphab <= 'Z' ; alphab++);
+for (alphab = 'A' ; alphab <= 'Z' ; alphab++)
 {
 putchar(alphab);
 }
