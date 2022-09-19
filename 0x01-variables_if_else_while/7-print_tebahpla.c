aca9a75b35642643fa656fadab19b018ed3dbe6b
esenam67 <moneyesenam@gmail.com>
2022-09-19 06:40:04 -0700

    k

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - reverse number
+ *
+ * Description: the number in reverse
+ * the program prints reverse
+ * Return: 0
+ */
+int main(void)
+{
+char rev;
+for (rev = 'z' ; rev <= 'a' ; rev++)
+{
+putchar(rev);
+}
+putchar("\n");
+return (0);
