a243a511bd9de7a37fe99f78f3a47337668a641a
esenam67 <moneyesenam@gmail.com>
2022-09-19 06:01:46 -0700

    u

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print numbers
+ *
+ * Description: using main function
+ * this will print digit of base ten
+ * Return
+ */
+int main(void)
+{
+int n
+for (n = '0' ; n < 10 ; n+++)
+{
+printf("%d\n", n);
+}
+return (0);
+}
