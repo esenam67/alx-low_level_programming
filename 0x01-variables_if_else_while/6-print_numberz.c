6fce644128ae92ccd3f1c17187eca22d156a8080
esenam67 <moneyesenam@gmail.com>
2022-09-19 06:30:33 -0700

    n

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - numbers
+ *
+ * Description: base 10 digits
+ * the program ptints digits
+ * Return: 0
+ */
+int main(void)
+{
+int num;
+for (num = 0 ; num < 10 ; num++)
+{
+putchar(num);
+}
+putchar("\n")
+return (0);
+}
