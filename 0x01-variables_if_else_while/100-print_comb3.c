75edef03b8d1d8f1ef35e02f34906bc6962d9a43
esenam67 <moneyesenam@gmail.com>
2022-09-20 08:29:29 -0700

    d

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,16 +6,16 @@
  * this program would print comb of two digits
  * Return: 0
  */
-int main()
+int main(void)
 {
 int comb2, comb3;
-for (comb2 = 0; comb2 < 10; comb2++)
+for (comb2 = 0; comb2 < 9; comb2++)
 {
-for (comb3 = 0; comb3 < 10; comb3++)
+for (comb3 = comb2 + 1; comb3 < 10; comb3++)
 {
 putchar((comb2 % 10) + '0');
 putchar((comb3 % 10) + '0');
-if (comb2 == 9 && comb3 == 9)
+if (comb2 == 8 && comb3 == 9)
 continue;
 putchar(',');
 putchar(' ');
