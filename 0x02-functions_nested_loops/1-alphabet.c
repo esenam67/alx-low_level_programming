116969df6924e5b284e207aad44131fb721bc0cc
esenam67 <moneyesenam@gmail.com>
2022-09-21 04:21:44 -0700

    j

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+ * main - alphabet
+ *
+ * Description: prints alphabet
+ * the programprins alphabets
+ * Return: 0
+ */
+int main(void)
+{
+char k;
+for (k = 'a'; k <= 'z'; k++)
+{
+_putchar('k');
+_putchar('\n');
+}
+return (0);
+}
