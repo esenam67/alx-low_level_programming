f6d1658f711d828b220e456f45cee4f7d33c07d6
esenam67 <moneyesenam@gmail.com>
2022-09-22 14:58:00 -0700

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - function that checks upper char
+ * @c: iput
+ * Return: 1 if c is uppercase, else 0 
+ */
+int _upper(int c)
+if ((c >= 'A') && (c <= 'z')
+return (1);
+else
+return (0);
+}
