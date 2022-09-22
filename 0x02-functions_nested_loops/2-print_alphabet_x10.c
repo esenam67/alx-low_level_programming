ac172483fda245c4581892f6e97144faa914b652
esenam67 <moneyesenam@gmail.com>
2022-09-21 18:42:00 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+int k, x;
+for (k = 10; k <= 10;  k++)
+{
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar (x);
+}
+_putchar ('\n');
+}
+}
