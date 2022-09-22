d0cdeb1c46ecae073eee67df4fa2372c405d7623
esenam67 <moneyesenam@gmail.com>
2022-09-21 19:35:36 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - check code
+ *
+ * Desription: using main fonction
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+elae
+{
+_putchar('-');
+return (-1);
+}
