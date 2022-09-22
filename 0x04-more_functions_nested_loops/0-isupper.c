0a1314aeb6fb106e4c61156b17ef065ef43ca36b
esenam67 <moneyesenam@gmail.com>
2022-09-22 15:41:10 -0700

    a

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,9 @@
  * @c: iput
  * Return: 1 if c is uppercase, else 0
  */
-int _upper(int c)
+int _isupper(int c)
 {
-if ((c >= 'A') && (c <= 'z')
+if ((c >= 'A') && (c <= 'Z'))
 return (1);
 else
 return (0);
