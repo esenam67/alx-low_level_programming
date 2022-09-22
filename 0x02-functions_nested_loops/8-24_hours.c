bfb48acb55c3628403c49789516556fb12584d37
esenam67 <moneyesenam@gmail.com>
2022-09-21 20:30:36 -0700

    a

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ *
+ * Description: using main funcion
+ */
+void jack_bauer(void)
+{
+int h, j;
+for (h = 0; h < 24; h++)
+{
+for (j = 0; j < 60; j++)
+{ 
+if ( h < 10)
+{
+_putchar('0');
+_putchar(h + '0');
+}
+else if (h >= 10)
+{
+_putchar((h / 10) + '0');
+_putchar((h % 10) + '0');
+}
+if (j < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(j + '0');
+}
+else if (j > 10)
+{
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+}
